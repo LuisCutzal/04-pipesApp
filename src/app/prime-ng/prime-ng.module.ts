6ac709163989c8368543535dae5ac5603c2ee513
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2021-05-07 12:20:21 -0600

    135. PrimeNg - MenuBar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import {CardModule} from 'primeng/card';
 import {ButtonModule} from 'primeng/button';
 
+import {MenubarModule} from 'primeng/menubar';
+
 
 
 @NgModule({
   declarations: [],
   exports: [
     CardModule,
-    ButtonModule
+    ButtonModule,
+    MenubarModule,
+    
   ]
 })
 export class PrimeNgModule { }
