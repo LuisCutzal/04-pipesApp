8d4ed37850adb78ae8db7f17023aedd42935f744
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2021-06-02 22:28:04 -0600

    147. I18nPluralPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,13 +8,22 @@ import { Component} from '@angular/core';
 })
 export class NoComunesComponent {
 
+  //el genero tiene que hacer match con la invitacionMapa, si es masculino mostrara invitarlo, si es femenino mostrara invitarla
+  //i18nSelect
   nombre:string="Luis";
   genero:string="masculino";
 
   invitacionMapa={
     "masculino":"invitarlo",
     "femenino":"invitarla"
-  }
+  };
 
-  //el genero tiene que hacer match con la invitacionMapa, si es masculino mostrara invitarlo, si es femenino mostrara invitarla
+  //i18nPlural
+  clientes : string[] = ['Maria','Pedro','Juan',"Susana","Kevin","Fernando"]; //arreglo de clientes
+  clientesMap={ //se tiene que pensar en las diversas opciones que puede tener
+    '=0':'no tenemos ningun cliente esperando.',
+    '=1':'tenemos 1 clinete esperando.',
+    '=2':'tenemos 2 clientes esperando.',
+    'other':'tenemos # clientes esperando'
+  }
 }
