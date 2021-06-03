dab26237d675d99670facc25b930f1e42c9ca2d0
LuisCutzal <2842258270404@ingenieria.usac.edu.gt>
2021-06-02 23:50:32 -0600

    152. AsyncPipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -70,4 +71,12 @@ export class NoComunesComponent {
     }
   ]
 
+  //Async Pipe
+  miObservable=interval(1000);//muestra 0,1,2,3,4....
+
+  valorPromesa=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+      resolve('Tenemos datos de la promesa')
+    },3500);
+  });
 }
